564feadd7f750334707d23a048f2b7889e3c00fb
Anabelle Gauvreau <gauvreaua7003@my.sduhsd.net>
2019-05-23 10:21:23 -0700

    iiteration

---
+++ cdemo/iteration.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+   int div = 2;
+   int n, mod;
+   int c;
+   for (int i = 1; i<100; i++)
+   {
+     mod = div % i;
+	if (mod = 0)
+	{
+	  printf("i\n");
+	}
+   }
+}
