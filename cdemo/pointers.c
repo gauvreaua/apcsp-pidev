37c7bfd3e24e5819389ee26696348216e98456a2
Anabelle Gauvreau <gauvreaua7003@my.sduhsd.net>
2019-05-28 10:19:38 -0700

    pointers

--- cdemo/pointers.c
+++ cdemo/pointers.c
@@ -17,4 +17,15 @@ int main()
   printf("The value of ptrtoa is %d\n", ptrtoa);
   printf("It stores the value %d\n", *ptrtoa);
   printf("The address of a is %d\n", &a);
+
+  float d = 1.2;
+  float* ptrtod;
+  ptrtod = &d;
+  *ptrtod = 3;
+  float e = 4.1;
+  float* ptrtoe;
+  ptrtoe = &e;
+  *ptrtoe = 8;
+  printf("The value of d is %f and the address of d is %d\n", d, &d);
+  printf("The value of e is %f and the address of e is %d\n", e, &e);
 }
